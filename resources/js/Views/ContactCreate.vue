7cd8e39b844fa1798a34a6c5e1f3d61616e3f0f5
Volodymyr Komarov <vovikems@gmail.com>
2021-03-26 17:09:20 +0200

    Implement contact creating

---
+++ resources/js/Views/ContactCreate.vue
@@ -0,0 +1,18 @@
+<template>
+    <b-container>
+        <div class="row">
+            <div class="col-md-8 ml-md-auto mr-md-auto">
+                <contact-create></contact-create>
+            </div>
+        </div>
+    </b-container>
+</template>
+
+<script>
+import ContactCreate from '../Components/Contacts/ContactCreate.vue';
+
+export default {
+    name: 'login',
+    components: { ContactCreate },
+}
+</script>
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [LoginController::class, 'login'])->name('auth.login');
 Route::any('/logout', [LogoutController::class, 'logout'])->name('auth.logout');
 
-Route::get('/{any?}', function () {
+Route::fallback(function () {
     return view('welcome');
 });
